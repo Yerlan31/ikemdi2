e46a91bfd4a0449ba09fa5c3859a39b40da5fd88
Nazirov Ilhomjon <naziroffjr@gmail.com>
2024-07-14 00:42:36 +0300

    add cors

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -3,37 +3,39 @@ import * as cors from 'cors';
 import router from './routes';
 
 class App {
-  public app: express.Express;
+    public app: express.Express;
 
-  constructor() {
-    this.app = express();
-    this.config();
-  }
+    constructor() {
+        this.app = express();
+        this.config();
+    }
 
-  private config():void {
-    const accessControl: express.RequestHandler = (_req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
-      res.header('Access-Control-Allow-Headers', '*');
-      next();
-    };
+    private config(): void {
+        const accessControl: express.RequestHandler = (_req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+            res.header('Access-Control-Allow-Headers', '*');
+            res.header("Access-Control-Allow-Credentials", "true");
 
-    this.app.use(express.json({limit: '50mb'}));
-    this.app.use(express.urlencoded({limit: '50mb'}));
-    this.app.use(cors());
-    this.app.use(accessControl);
-    this.app.use(express.json());
-    this.app.use(router);
- }
+            next();
+        };
 
-  public start(PORT: string | number):void {
-    this.app.listen(PORT, () => {
-      console.log(`App listening on the port ${PORT}`);
-    });
-  }
+        this.app.use(express.json({limit: '50mb'}));
+        this.app.use(express.urlencoded({limit: '50mb'}));
+        this.app.use(accessControl);
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(router);
+    }
+
+    public start(PORT: string | number): void {
+        this.app.listen(PORT, () => {
+            console.log(`App listening on the port ${PORT}`);
+        });
+    }
 }
 
-export { App };
+export {App};
 
 // A execução dos testes de cobertura depende dessa exportação
-export const { app } = new App();
+export const {app} = new App();
