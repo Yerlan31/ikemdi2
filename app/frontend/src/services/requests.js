ffb88637713e54fa5ec4fd0f10cafeefd155c875
Nazirov Ilhomjon <naziroffjr@gmail.com>
2024-07-14 00:33:56 +0300

    change ip back

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://91.147.92.130:3001/`,
+  baseURL: `http://91.147.92.130:3001`,
 });
 
 export const setToken = (token) => {
